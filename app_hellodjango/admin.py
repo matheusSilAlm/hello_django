68d7062f9827183766c1438ade76288a033569a4
Matheus Silva <teushiftz@gmail.com>
2023-05-19 00:42:22 -0300

    tchau

--- app_hellodjango/admin.py
+++ app_hellodjango/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app_hellodjango.models import evento
 
 # Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento','data_criacao')
+    list_filter = ('usuario','data_evento')
+
+admin.site.register(evento,EventoAdmin)
