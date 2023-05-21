77ee63e76dfac61ad5fd1a445b0aabb406aff9d3
Matheus Silva <teushiftz@gmail.com>
2023-05-20 21:06:58 -0300

    ultimas mudancas 3

--- app_hellodjango/views.py
+++ app_hellodjango/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render, redirect
 from app_hellodjango.models import evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
 
-# Create your views here.
 # def hello(request,nome):
-    
 #     return HttpResponse('<h1> Hello {} </h1>'.format(nome)
 # def index(request):
 #     return redirect('/agenda/')
 
+def login_user(request):
+   return render(request, 'login.html')
+
+def logout_user(request):
+    logout (request)
+    return redirect('/')
+
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request,usuario)
+            return redirect('/')
+        else:
+            messages.error(request,'Usuario ou senha invalidos') 
+    return redirect('/')
+    
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     # usuario = request.user
-    Evento = evento.objects.all()     #filter(usuario=usuario)
+    usuario = request.user
+    Evento = evento.objects.filter(usuario=usuario)    #filter(usuario=usuario)
     response = {'eventos': Evento}
     return render(request, 'agenda.html',response)
+
+
     
    
